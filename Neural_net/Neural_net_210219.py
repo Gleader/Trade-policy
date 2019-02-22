48fbdbadaa0d00b55842aa7c1b239df0eb4c3d88
Gleader <40356698+Gleader@users.noreply.github.com>
2019-02-22 17:08:19 +0100

    Update Neural_net_210219.py

--- Neural_net/Neural_net_210219.py
+++ Neural_net/Neural_net_210219.py
@@ -2,7 +2,7 @@
 # Neural net created for the gravity model prediction for the Trade Policy class at PSE
 # Author: Michal Miktus at michal.miktus@gmail.com
 # Date: 19.02.2019
-
+#
 # Import libraries
 
 import plotly.io as pio
